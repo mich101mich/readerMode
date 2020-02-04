7eea2ffb940d3aede1344d02ef419dcb721c5973
mich101mich <mich101mich@gmail.com>
2020-02-04 21:10:16 +0100

    added self-disable on inactive

--- readerMode.js
+++ readerMode.js
@@ -17,7 +17,18 @@ chrome.storage.sync.get('blacklist', ({ blacklist }) => {
 	};
 
 	/** @type { { [key: string]: boolean } } */
-	const keyDown = {};
+	const keyDown = {
+		"control": false,
+		"shift": false,
+		"alt": false,
+		"meta": false,
+	};
+	for (const key in map) {
+		keyDown[key] = false;
+	}
+
+	// requestAnimationFrame has been called
+	let active = false;
 
 	/** @type { Element | Window } */
 	let defaultTarget = window;
@@ -94,11 +105,16 @@ chrome.storage.sync.get('blacklist', ({ blacklist }) => {
 			return;
 		}
 		specialKeys(e);
-		if (!keyboardUsed()) {
-			const key = e.key.toLowerCase();
-			keyDown[key] = true;
-			if (key in map) {
-				e.preventDefault();
+		const key = e.key.toLowerCase();
+		if (keyboardUsed() || !(key in keyDown)) {
+			return;
+		}
+		keyDown[key] = true;
+		if (key in map) {
+			e.preventDefault();
+			if (!active) {
+				requestAnimationFrame(moveStuff);
+				active = true;
 			}
 		}
 	});
@@ -107,19 +123,31 @@ chrome.storage.sync.get('blacklist', ({ blacklist }) => {
 		if (!e.key) { // apparently this is a thing...
 			return;
 		}
-		const key = e.key.toLowerCase();
-		keyDown[key] = false;
 		specialKeys(e);
+		const key = e.key.toLowerCase();
+		if (!(key in keyDown)) {
+			return;
+		}
+		keyDown[key] = false;
 		if (!keyboardUsed() && key in map) {
 			e.preventDefault();
 		}
 	});
 
 	const moveStuff = function () {
-		requestAnimationFrame(moveStuff);
+		active = false;
 
 		if (keyboardUsed()) {
 			return;
+		}
+
+		for (const key in map) {
+			if (keyDown[key]) {
+				active = true;
+			}
+		}
+		if (active) {
+			requestAnimationFrame(moveStuff);
 		}
 
 		let scrollX = 0;
@@ -142,7 +170,6 @@ chrome.storage.sync.get('blacklist', ({ blacklist }) => {
 		let factor = keyDown["shift"] ? 3 : 1;
 		target.scrollBy(scrollX * window.readerModeScrollSpeed * factor, scrollY * window.readerModeScrollSpeed * factor);
 	};
-	moveStuff();
 
 	console.log("Reader Mode " + chrome.runtime.getManifest().version + " enabled");
 
